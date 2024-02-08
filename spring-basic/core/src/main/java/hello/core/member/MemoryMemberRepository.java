3a65deb96fe1b63a6fe4651087271b3cccba7652
yeonjae02 <0yeonjae2@naver.com>
2024-02-08 20:14:37 +0900

    feat : 스프링 핵심원리-기본편 섹션 2 코드 업로드

---
+++ spring-basic/core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,21 @@
+package hello.core.member;
+
+import org.apache.commons.logging.Log;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    // 실무에서는 동시성 이슈 고려
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
