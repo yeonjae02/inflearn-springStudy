9cd1cf2ea07b9dea396ac11def77b4086cda1aaa
yeonjae02 <0yeonjae2@naver.com>
2024-02-28 22:41:46 +0900

    feat : 스프링 핵심원리-기본편 섹션 7 코드 업로드

--- spring-basic/core/src/main/java/hello/core/AutoAppConfig.java
+++ spring-basic/core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,7 +1,13 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +23,14 @@ import org.springframework.context.annotation.FilterType;
 // basePackage 미지정 시 hello.core에서 시작
 public class AutoAppConfig {
 
+//    @Autowired MemberRepository memberRepository;
+//    @Autowired DiscountPolicy discountPolicy;
+//
+//    @Bean
+//    OrderService orderService() {
+//        return new OrderServiceImpl(memberRepository,discountPolicy );
+//    }
+//
 //    @Bean(name="memoryMemberRepository")
 //    MemberRepository memberRepository(){
 //        return new MemoryMemberRepository();
--- spring-basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring-basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,21 +1,26 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+//@RequiredArgsConstructor // final 키워드가 있는 것들에 대해 생성자 만들어줌.
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    // private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
-@Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    @Autowired private DiscountPolicy rateDiscountPolicy;
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy){
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
